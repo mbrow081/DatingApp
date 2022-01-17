a3c3c84cbe59b831a773da27f189b9ee398ec10a
Michael Brown <brownm@harringtonhoists.com>
2022-01-17 11:39:32 -0500

    End of Section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
